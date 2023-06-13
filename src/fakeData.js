09e0a10e1fea96924a9d9b6f0d0a844f31f514bc
Lourenço Viguini Werneck <lourencovw@gmail.com>
2023-06-13 16:49:55 -0300

    Add missing data

--- src/fakeData.js
+++ src/fakeData.js
@@ -1,39 +1,47 @@
-export const fakeScript = [
-    {
-        message: {
-            role: 'assistant',
-            content: 'Host:\n' +
-                '\n' +
-                'Welcome to our special presentation on the history of bacon. Today, we’ll be exploring the origins of this mouth-watering food and the evolution of its popularity over the centuries.\n' +
-                '\n' +
-                'Narrator:\n' +
-                '\n' +
-                "Bacon has been a staple in many people's diets for centuries, but do you know when it first appeared on the scene? The earliest records of curing pork dates back to around 1500 BC. The ancient Chinese and Romans both had bacon-like products, and the word ‘bacon’ comes from the Middle English word ‘bakoun’ meaning ‘back meat.’ However, the bacon we are familiar with today was traditionally made in Europe in the 16th century.\n" +
-                '\n' +
-                'Host:\n' +
-                '\n' +
-                'As bacon found its way into kitchens around the world, its popularity grew steadily, and by the Industrial Revolution, bacon became much more widely available. There were numerous ways of curing and smoking bacon, which varied from region to region. In the United States, bacon had an initial surge in popularity during the colonial era, but it wasn’t until the 20th century that Americans truly fell in love with bacon.\n' +
-                '\n' +
-                'Narrator:\n' +
-                '\n' +
-                'During World War II, the government heavily promoted pork, and this helped to increase its consumption. At the time, bacon was essential for feeding the soldiers, and it wasn’t long before many people began to realize how great it tasted in all kinds of meals. The post-war boom, combined with an increase in mass marketing, helped to cement bacon’s popularity in the United States.\n' +
-                '\n' +
-                'Host:\n' +
-                '\n' +
-                'But what are some of the dishes that helped make bacon famous? At breakfast time, bacon and eggs became the classic American breakfast dish enjoyed all across the country. The BLT sandwich, with its bacon, lettuce, and tomato, is another dish that’s become synonymous with bacon.\n' +
-                '\n' +
-                'Narrator:\n' +
-                '\n' +
-                'In recent years, bacon has been used in all kinds of recipes, including desserts. From bacon-wrapped dates to bacon caramel brownies, chefs are finding new ways to incorporate bacon into their dishes, and its popularity continues to grow.\n' +
-                '\n' +
-                'Host:\n' +
-                '\n' +
-                'And that’s a brief history of bacon. Whether you like it crispy or chewy, on its own or as part of a dish, bacon is a true culinary delight. Thanks for joining us in this special presentation, and we hope you continue to enjoy the delicious taste of bacon for many years to come.'
-        },
-        finish_reason: 'stop',
-        index: 0
-    }
-];
+export const fakeScript = {
+    id: 'chatcmpl-7Q0eur0vX6c1XGHj2kOObprebvhUl',
+    object: 'chat.completion',
+    created: 1686433416,
+    model: 'gpt-3.5-turbo-0301',
+    usage: { prompt_tokens: 16, completion_tokens: 517, total_tokens: 533 },
+    choices: [
+        {
+            message: {
+                role: 'assistant',
+                content: 'Host:\n' +
+                    '\n' +
+                    'Welcome to our special presentation on the history of bacon. Today, we’ll be exploring the origins of this mouth-watering food and the evolution of its popularity over the centuries.\n' +
+                    '\n' +
+                    'Narrator:\n' +
+                    '\n' +
+                    "Bacon has been a staple in many people's diets for centuries, but do you know when it first appeared on the scene? The earliest records of curing pork dates back to around 1500 BC. The ancient Chinese and Romans both had bacon-like products, and the word ‘bacon’ comes from the Middle English word ‘bakoun’ meaning ‘back meat.’ However, the bacon we are familiar with today was traditionally made in Europe in the 16th century.\n" +
+                    '\n' +
+                    'Host:\n' +
+                    '\n' +
+                    'As bacon found its way into kitchens around the world, its popularity grew steadily, and by the Industrial Revolution, bacon became much more widely available. There were numerous ways of curing and smoking bacon, which varied from region to region. In the United States, bacon had an initial surge in popularity during the colonial era, but it wasn’t until the 20th century that Americans truly fell in love with bacon.\n' +
+                    '\n' +
+                    'Narrator:\n' +
+                    '\n' +
+                    'During World War II, the government heavily promoted pork, and this helped to increase its consumption. At the time, bacon was essential for feeding the soldiers, and it wasn’t long before many people began to realize how great it tasted in all kinds of meals. The post-war boom, combined with an increase in mass marketing, helped to cement bacon’s popularity in the United States.\n' +
+                    '\n' +
+                    'Host:\n' +
+                    '\n' +
+                    'But what are some of the dishes that helped make bacon famous? At breakfast time, bacon and eggs became the classic American breakfast dish enjoyed all across the country. The BLT sandwich, with its bacon, lettuce, and tomato, is another dish that’s become synonymous with bacon.\n' +
+                    '\n' +
+                    'Narrator:\n' +
+                    '\n' +
+                    'In recent years, bacon has been used in all kinds of recipes, including desserts. From bacon-wrapped dates to bacon caramel brownies, chefs are finding new ways to incorporate bacon into their dishes, and its popularity continues to grow.\n' +
+                    '\n' +
+                    'Host:\n' +
+                    '\n' +
+                    'And that’s a brief history of bacon. Whether you like it crispy or chewy, on its own or as part of a dish, bacon is a true culinary delight. Thanks for joining us in this special presentation, and we hope you continue to enjoy the delicious taste of bacon for many years to come.'
+            },
+            finish_reason: 'stop',
+            index: 0
+        }
+    ]
+}
+    ;
 
 export const fakeImages = {
     items: [
