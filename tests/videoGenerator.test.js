79af2e844b53627ffa8befa2a51f9198fec9a855
Lourenço Viguini Werneck <lourencovw@gmail.com>
2023-06-13 16:49:08 -0300

    Set up tests

---
+++ tests/videoGenerator.test.js
@@ -0,0 +1,8 @@
+import { fakeScript } from '../src/fakeData.js';
+import VideoGenerator from './src/VideoGenerator.js';
+
+test('string with a single number should result in the number itself', () => {
+    const videoGenerator = new VideoGenerator('test')
+    videoGenerator.script = fakeScript
+    videoGenerator.
+  });
