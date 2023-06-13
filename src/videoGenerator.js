1af20cd208a56ba1d04fbe309104d27f4f81c9a1
Lourenço Viguini Werneck <lourencovw@gmail.com>
2023-06-13 16:50:28 -0300

    Add class

--- src/videoGenerator.js
+++ src/videoGenerator.js
@@ -1,66 +1,102 @@
-const ffmpeg = require('fluent-ffmpeg');
+class VideoGenerator {
+    constructor(theme) {
+        this.theme = theme
+        this.editSpec = {
+            outPath: './result/speedTest.mp4',
+            width: 1280,
+            height: 720,
+            defaults: {
+                transition: { name: 'crossZoom', duration: 1 }
+            },
+            clips: [
+                { duration: 3, layers: [{ type: 'image', path: './temp/img1.jpg', zoomDirection: 'out' }] },
+                { duration: 3, layers: [{ type: 'image', path: './temp/img1.jpg', zoomDirection: 'in' }] },
+            ],
+        }
+        this.script = '';
+    }
 
-// Input video path
-const videoPath = 'path/to/input/video.mp4';
+    async getImagesFromScript() {
+        const newScript = []
+        const resp = await fetch('https://customsearch.googleapis.com/customsearch/v1?' + new URLSearchParams({
+            cx: process.env.MECHANISM_ID,
+            key: process.env.GOOGLE_KEY,
+            q: 'chicken',
+            searchType: 'image',
+            num: 2
+        }))
 
-// Input image path
-const imagePath = 'path/to/input/image.jpg';
+        const json = await resp.json();
 
-// Output video path
-const outputPath = 'path/to/output/video.mp4';
+        for (let i = 0; i < this.scrip.length; i++) {
+            const isEven = number % 2 == 0
+            if (isEven) {
+                newScript.push(this.scrip[i])
+            } else {
+                const imageSearch = this.scrip[i].toLowerCase().replace('imgdesc:', '');
 
-// Zoom parameters
-const zoomInScale = 1.5;  // Zoom in by 1.5x
-const zoomOutScale = 0.5; // Zoom out by 0.5x
+                await progress(request(imageSearch.trim()))
+                    .on('progress', progress => console.log("progress", progress.percent))
+                    .on('response', response => console.log("status code", response.statusCode))
+                    .on('error', error => console.error("error", error))
+                    .on('end', () => console.log("done"))
+                    .pipe(fs.createWriteStream(path))
+            }
 
-// Pan parameters
-const panLeftOffset = 100;   // Pan left by 100 pixels
-const panRightOffset = -100; // Pan right by 100 pixels
+        }
 
-ffmpeg()
-    .input(videoPath)
-    .input(imagePath)
-    .complexFilter([
-        // Zoom in
-        {
-            filter: 'zoompan',
-            options: `z='if(lte(zoom,1.0),${zoomInScale},1.0)':x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':d=1:s=WxH`,
-            inputs: '0:v',
-            outputs: 'zoomedInVideo',
-        },
-        // Zoom out
-        {
-            filter: 'zoompan',
-            options: `z='if(lte(zoom,1.0),${zoomOutScale},1.0)':x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':d=1:s=WxH`,
-            inputs: 'zoomedInVideo',
-            outputs: 'zoomedOutVideo',
-        },
-        // Pan left
-        {
-            filter: 'crop',
-            options: `out_w=in_w-2*${panLeftOffset}:out_h=in_h:x=${panLeftOffset}:y=0`,
-            inputs: 'zoomedOutVideo',
-            outputs: 'panLeftVideo',
-        },
-        // Pan right
-        {
-            filter: 'crop',
-            options: `out_w=in_w-2*${panRightOffset}:out_h=in_h:x=${panRightOffset}:y=0`,
-            inputs: 'panLeftVideo',
-            outputs: 'panRightVideo',
-        },
-        // Overlay image
-        {
-            filter: 'overlay',
-            options: 'x=(W-w)/2:y=(H-h)/2',
-            inputs: ['panRightVideo', '1:v'],
-            outputs: 'outputVideo',
-        },
-    ])
-    .outputOptions('-c:v libx264') // Output video codec
-    .outputOptions('-preset ultrafast') // Output video encoding preset (optional)
-    .output(outputPath)
-    .on('end', () => {
-        console.log('Video processing finished');
-    })
-    .run();
+        this.script = newScript;
+
+        return this
+    };
+
+    async getScript() {
+        // const CHATGPT_URL = "https://api.openai.com/v1/chat/completions";
+        // const HEADERS = {
+        //     Accept: "application/json",
+        //     "Content-Type": "application/json",
+        //     Authorization: "Bearer " + process.env.GPT_KEY,
+        // };
+        // const MODEL_CONFIG = JSON.stringify({
+        //     "model": "gpt-3.5-turbo",
+        //     "messages": [{
+        //         "role": "user",
+        //         "content": `Create a text with the theme "${this.theme}", ` +
+        //             `and for each paragraph describe an image separately with three words with a indicator "imgdesc"`
+        //     }]
+        // })
+
+        // const resp = await fetch(CHATGPT_URL, { method: "POST", headers: HEADERS, body: MODEL_CONFIG })
+        // const json = await resp.json()
+
+        // this.script = json?.choices[0]?.message?.content;
+        this.scrip = [
+            'Pigs are fascinating creatures that have captivated the attention of people all around the world. These intelligent animals are known for their curly tails, snouts, and floppy ears. In the world of biology, pigs take center stage due to their unique anatomy and physiology. ',
+            'Imgdesc: Curly tails, snouts, floppy ears.',
+            'Pigs belong to the family Suidae and are native to Eurasia. They are omnivorous, which means they eat both plants and animals. Pigs can consume a variety of foods such as vegetables, fruits, insects, and even small animals. Pigs have a four-chambered stomach which allows them to digest complex foods. ',
+            'Imgdesc: Omnivorous, four-chambered stomach.',
+            'There are different breeds of pigs, each with its unique characteristics. Berkshire, Duroc, Hampshire, and Yorkshire are some of the most common breeds of pigs. Pigs can weigh up to 700 pounds and can live for up to 15 years. ',
+            'Imgdesc: Berkshire, Duroc, Hampshire, Yorkshire.',
+            'Pigs are highly social animals and enjoy the company of other pigs. In the wild, they live in groups known as sounders. Pigs communicate with each other using various sounds such as grunts, snores, and squeals. They also use body language to express themselves. ',
+            'Imgdesc: Highly social, sounders, communication.',
+            'Pigs have a well-developed sense of smell and taste. Their snouts are specially adapted to help them root through the ground in search of food. Pigs are also very clean animals and have been known to roll in mud to protect themselves against insects and sunburn. ',
+            'Imgdesc: Well-developed senses, snouts, mud-roll.',
+            'In conclusion, pigs are fascinating creatures with a unique biology that sets them apart from other animals. Their omnivorous nature, four-chambered stomach, and social behavior make them an important species to study in the fields of biology and animal science. ',
+            'Imgdesc: Fascinating creatures, biology, animal science.'
+        ]
+        console.log(this.script.split('\n\n'));
+        return this;
+    }
+
+    async downloadImage() {
+        return
+    }
+
+
+    createVideo() {
+        editly(this.editSpec)
+    }
+
+}
+
+export default VideoGenerator;
